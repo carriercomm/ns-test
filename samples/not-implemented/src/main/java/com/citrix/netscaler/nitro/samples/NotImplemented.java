76b09d5058ac699a1fef5c6a4bce586abcd52ec5
Carlos D. Garza <carlos.garza@rackspace.com>
2013-06-26 21:51:13 -0500

    Adding NotImplemented assembly as a template for the other Sample Classes.

---
+++ samples/not-implemented/src/main/java/com/citrix/netscaler/nitro/samples/NotImplemented.java
@@ -0,0 +1,7 @@
+package com.citrix.netscaler.nitro.samples;
+
+public class NotImplemented{
+    public static void main(String[] args){
+        System.out.printf("Main class not implemented\n");
+    }
+}
