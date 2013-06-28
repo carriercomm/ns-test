71cb3a06856537a7510dbdec4ea0b4488b858965
Carlos D. Garza <carlos.garza@rackspace.com>
2013-06-28 07:15:14 -0500

    Adding run_bat jar.

---
+++ samples/sample-classes/src/main/java/com/citrix/netscaler/nitro/samples/run_bat.java
@@ -0,0 +1,14 @@
+
+package com.citrix.netscaler.nitro.samples;
+
+public class run_bat {
+    public static void main(String[] args){
+        set_config.main(args);
+        get_config.main(args);
+        stats.main(args);
+        rm_config.main(args);
+        MyFirstNitroApplication.main(args);
+        CreateCluster.main(args);
+    }
+
+}
